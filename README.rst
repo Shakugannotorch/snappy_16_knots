ef88d916f0e4e0536001ece843555d78cf6626f9
Shana <903443276@qq.com>
2026-01-08 23:15:54 -0600

    First commit

---
+++ README.rst
@@ -0,0 +1,56 @@
+The SnapPy manifold database for 16 crossings knots
+==========================================================
+
+This repository stores the manifold databases for 16 crossing
+knots, and includes the source code for the Python module
+:code:`snappy_16_knots` which packages them up for use in SnapPy and
+Spherogram. 
+
+To install this module in SageMath::
+
+  sage -pip install git+https://github.com/Shakugannotorch/snappy_16_knots/
+
+A prerequisite for using :code:`snappy_16_knots` is :code:`snappy_15_knots`; the above command
+should be able to automatically install :code:`snappy_15_knots`, if it is not readily installed.
+
+To use this module with SnapPy, one can do::
+
+  sage: from snappy_16_knots import snappy
+
+The extended census can then be accessed via SnapPy's :code:`Manifold` class. 
+For example::
+
+  sage: m = snappy.Manifold('K16a101')
+  sage: m.triangulation_isosig()
+  'yLLLPLLAwMLzwQQLQcbehfiljimnonrqrsstuvwxxxdjqdgtehqxqajhfuajoffaacv_bacB'
+
+  sage: m = snappy.Manifold('K16a101(2,3)')
+  sage: m.triangulation_isosig()
+  ''yLLLPLLAwMLzwQQLQcbehfiljimnonrqrsstuvwxxxdjqdgtehqxqajhfuajoffaacv_bacB(2,3)''
+
+The iterator for all manifolds in this module, along with those in 
+:code:`snappy_15_knots`, is :code:`snappy.ExtendedHTLinkExteriors`. 
+For example::
+
+  sage: len(snappy.ExtendedHTLinkExteriors)
+  1822509
+
+  sage: for M in snappy.ExtendedHTLinkExteriors[-9:-6]: print(M, M.volume()) 
+  K16n1008898(0,0) 22.8613896980723
+  K16n1008899(0,0) 17.0540820108716
+  K16n1008900(0,0) 16.9295548661239
+
+  sage: for M in snappy.ExtendedHTLinkExteriors(num_cusps=2)[-3:]: print(M, M.volume(), M.num_cusps())
+  L14n40046(0,0)(0,0) 18.6813172532672 2
+  L14n40047(0,0)(0,0) 14.8257607028697 2
+  L14n40048(0,0)(0,0) 23.2884335333958 2
+
+The raw source for the tables are in::
+  
+  manifold_src/original_manifold_sources
+
+stored as plain text CSV files for the potential convenience of other
+users. The triangulations themselves are stored in the "isosig" format
+of Burton, as described in the appendix to `this paper
+<http://arxiv.org/abs/1110.6080>`_ with an added "decoration" suffix
+that describes the peripheral framing.
---
+++ python_src/database.py
@@ -0,0 +1,188 @@
+from __future__ import print_function
+import sys, sqlite3, re, os, random
+import snappy_15_knots
+
+# This module uses sqlite3 databases with multiple tables.
+# The path to the database file is specified at the module level.
+from .sqlite_files import __path__ as manifolds_paths
+manifolds_path = manifolds_paths[0]
+database_path = os.path.join(manifolds_path, '16_knots.sqlite')
+
+split_filling_info = re.compile('(.*?)((?:\([0-9 .+-]+,[0-9 .+-]+\))*$)')
+
+original_manifolds_path = snappy_15_knots.manifolds_path
+original_database_path = os.path.join(original_manifolds_path, '15_knots.sqlite')
+
+def get_tables(ManifoldTable):
+    """
+    Functions such as this one are meant to be called in the
+    __init__.py module in snappy proper.  To avoid circular imports,
+    it takes as argument the class ManifoldTable from database.py in
+    snappy. From there, it builds all of the Manifold tables from the
+    sqlite databases manifolds.sqlite and more_manifolds.sqlite in
+    manifolds_src, and returns them all as a list.
+    """
+
+    class LinkExteriorsTable(ManifoldTable):
+        """
+        Link exteriors usually know a DT code describing the assocated link.
+        """
+        _select = 'select name, triangulation, DT from %s '
+
+        def _finalize(self, M, row):
+            M.set_name(row[0])
+            M._set_DTcode(row[2])
+
+
+    class ExtendedHTLinkExteriors(LinkExteriorsTable):
+        """
+        This table extends the HTLinkExteriors table from snappy_manifolds
+        with 15 and 16 crossing knots (no links with multiple components).
+        They are ordered so that the HTLinkExteriors come first, then the 
+        alternating 15 and 16 crossing knots, then the nonalternating 15 and 
+        16 crossing knots.
+
+        >>> ExtendedHTLinkExteriors.identify(LinkExteriors['8_20'])
+        K8n1(0,0)
+        >>> Mylist = ExtendedHTLinkExteriors(alternating=False)[32.0:32.1]
+        >>> len(Mylist)
+        4
+        >>> for L in Mylist:
+        ...   print( L.name(), L.volume() )
+        ... 
+        ('K16n883306', 32.0352925448859)
+        ('K16n916708', 32.0248070639116)
+        ('K16n931674', 32.0386444405200)
+        ('K16n996627', 32.0366528043146)
+        """
+
+        _regex = re.compile('[KL][0-9]+[an]([0-9]+)$')
+        
+        def __init__(self, **kwargs):
+            LinkExteriorsTable.__init__(self,
+                                        table='HT_links_view',
+                                        db_path=original_database_path,
+                                        **kwargs)
+
+            view_name = 'all_HT_links_view'
+
+            conn = self._connection
+            cursor = conn.cursor()
+
+            # Dictionary specifying tables to append
+            # Keys: paths of databases -> Values: tables contained by given databases
+            # All tables specified will be appended to the view <view_name>
+            sql_dict = {
+                database_path : ['HT_links']
+            }
+
+            table_dict = dict() 
+            # records the aliases of databases containing a given table
+            alias_dict = dict() 
+            # records the alias of a given database file (1-to-1)
+
+            for i, sql_path in enumerate(sql_dict.keys(), start = 1):
+                alias = f'db{i}'
+                alias_dict.update({sql_path : alias})
+
+                for table_name in sql_dict[sql_path]:
+                    if table_name not in table_dict.keys():
+                        table_dict.update({table_name : [alias]})
+                    else:
+                        table_dict.update({table_name : table_dict[table_name] + [alias]})
+
+                cursor.execute('ATTACH DATABASE ? AS ?', (sql_path, alias))
+
+            select_statements = [f'SELECT * FROM {self._table}']
+            for table_name in table_dict.keys():
+                for alias in table_dict[table_name]:
+                    select_statements.append(f"SELECT * FROM {alias}.{table_name}")
+            union_query = ' UNION ALL '.join(select_statements)
+
+            create_view_sql = f"CREATE TEMPORARY VIEW {view_name} AS {union_query}"
+            cursor.execute(create_view_sql)
+            cursor.close()
+
+            self._table = view_name
+            self._select = f'select name, triangulation, DT, id from {view_name} '
+
+            self._get_length()
+            self._get_max_volume()
+                            
+        def _configure(self, **kwargs):
+            """
+            Process the ManifoldTable filter arguments and then add
+            the ones which are specific to links.
+            """
+            ManifoldTable._configure(self, **kwargs)
+            conditions = []
+
+            alt = kwargs.get('alternating', None)
+            if alt == True:
+                conditions.append("name like '%a%'")
+            elif alt == False:
+                conditions.append("name like '%n%'")
+            flavor = kwargs.get('knots_vs_links', None)
+            if flavor == 'knots':
+                conditions.append('cusps=1')
+            elif flavor == 'links':
+                conditions.append('cusps>1')
+            if 'crossings' in kwargs:
+                N = int(kwargs['crossings'])
+                conditions.append(
+                    "(name like '_%da%%' or name like '_%dn%%')"%(N,N))
+            if self._filter:
+                if len(conditions) > 0:
+                    self._filter += (' and ' + ' and '.join(conditions))
+            else:
+                self._filter = ' and '.join(conditions)
+
+    return [ExtendedHTLinkExteriors()]
+
+def connect_to_db(db_path):
+    """
+    Open the given sqlite database, ideally in read-only mode.
+    """
+    if sys.version_info >= (3,4):
+        uri = 'file:' + db_path + '?mode=ro'
+        return sqlite3.connect(uri, uri=True)
+    elif sys.platform.startswith('win'):
+        try:
+            import apsw
+            return apsw.Connection(db_path, flags=apsw.SQLITE_OPEN_READONLY)
+        except ImportError:
+            return sqlite3.connect(db_path)
+    else:
+        return sqlite3.connect(db_path)
+
+def get_DT_tables():
+    """
+    Returns two barebones databases for looking up DT codes by name. 
+    """
+    class DTCodeTable(object):
+        """
+        A barebones database for looking up a DT code by knot/link name.
+        """
+        def __init__(self, name='', table='', db_path=database_path, **filter_args):
+            self._table = table
+            self._select = 'select DT from ' + table + ' '
+            self.name = name
+            self._connection = connect_to_db(db_path)
+            self._cursor = self._connection.cursor()
+
+        def __repr__(self):
+            return self.name
+
+        def __getitem__(self, link_name):
+            select_query = self._select + ' where name="{}"'.format(link_name)
+            return self._cursor.execute(select_query).fetchall()[0][0]
+        
+        def __len__(self):
+            length_query = 'select count(*) from ' + self._table
+            return self._cursor.execute(length_query).fetchone()[0]
+
+
+    HTLinkDTcodesExtended = DTCodeTable(name='HTLinkDTcodesExtended',
+                                        table='HT_links_view',
+                                        db_path=database_path)
+    return [HTLinkDTcodesExtended]
