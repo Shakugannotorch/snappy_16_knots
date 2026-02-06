a0865c0ad86ba737db01b2f23f669c7e9eb433db
Shana <903443276@qq.com>
2026-02-05 16:28:05 -0800

    Avoid cicular import

--- python_src/__init__.py
+++ python_src/__init__.py
@@ -1,16 +1,6 @@
-__version__ = '1.17b'
+__version__ = '1.18b'
 
 def version():
     return __version__
 
 from .database import get_tables, get_DT_tables, manifolds_path, original_manifolds_path
-
-try:
-    import snappy
-    table_dict = snappy.database.add_tables_from_package('snappy_16_knots', False)
-    for name, table in table_dict.items():
-        setattr(snappy, name, table)
-        if name not in snappy.database_objects:
-            snappy.database_objects.append(name)
-except ImportError:
-    pass
